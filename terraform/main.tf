3b66eab037578cedb85d913a9fea334b80dc1093
Basil Peace <grv87@yandex.ru>
2019-11-17 20:54:20 +0300

    style: explicitly associate route table with subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -155,6 +155,11 @@ resource "aws_main_route_table_association" "a" {
   route_table_id = aws_route_table.r.id
 }
 
+resource "aws_route_table_association" "a" {
+  subnet_id = aws_subnet.fidata.id
+  route_table_id = aws_route_table.r.id
+}
+
 # Security Groups
 
 resource "aws_default_security_group" "default" {
